cd0db4c4f60138fb8d7c0db120c0a7210b8a79fe
Stas <zbara.dev@gmail.com>
2022-02-15 03:30:32 +0200

    fix error and then promise

--- client/js/script.js
+++ client/js/script.js
@@ -1,82 +1,63 @@
 ﻿(function () {
-        const getUserInfo = 'https://regiment.zbara.ru/friends/get/social';
-
+        let getUserInfo = 'https://regiment.zbara.ru/friends/get/social';
         let user = 0;
-
         let achievements = {
             "weapons": [0, 10, 25, 50, 100, 250, 500, 1000, 2500, 5000, 10000, 25000, 50000, 100000],
             "bosses": [0, 1, 5, 10, 25, 50, 100, 250, 500, 1000, 2500, 5000, 10000],
-            "boxes": [0, 1, 5, 10, 25, 50, 100, 250, 500, 1000, 2500, 5000, 10000],
-            "missions": [0, 1, 5, 10, 25, 50, 100, 250, 500, 1000, 2500],
             "sut": [0, 10, 25, 50, 100, 250, 500, 1000, 2500, 5000, 10000, 25000, 50000],
-            "exchange_collections": [0, 1, 5, 10, 25, 50, 100, 250, 500, 1000, 2500],
-            "send_help": [0, 1, 5, 10, 25, 50, 100, 250, 500, 1000, 2500, 5000, 10000],
             "coins": [0, 5000, 10000, 25000, 50000, 100000, 250000, 500000, 1000000, 2500000, 5000000],
             "tokens": [0, 250, 500, 1000, 2500, 5000, 10000, 25000, 50000, 100000, 250000, 500000],
             "encryptions": [0, 250, 500, 1000, 2500, 5000, 10000, 25000, 50000, 100000, 250000, 500000],
             "tickets": [0, 5, 10, 25, 50, 100, 250, 500, 1000, 2500, 5000, 10000, 25000, 50000],
             "damage": [100000, 250000, 500000, 1000000, 3000000, 5000000, 10000000, 20000000, 30000000, 50000000, 75000000, 100000000, 250000000, 500000000, 1000000000, 5000000000, 10000000000, 25000000000, 50000000000, 100000000000],
-            "open_package": [1, 5, 10, 25, 50, 100, 200, 300, 500, 750, 1000, 2000]
         };
         let bossList = {
             'win_boss_0': {
-                'name': 'Победить Йенеке',
-                'id': 0
+                'name': 'Йенеке',
             },
             'win_boss_1': {
-                'name': 'Победить Рундштедт',
-                'id': 1
+                'name': 'Рундштедт',
             },
             'win_boss_2': {
-                'name': 'Победить Манштейн',
-                'id': 2
+                'name': 'Манштейн',
             },
             'win_boss_3': {
-                'name': 'Победить Альмендингер',
-                'id': 3
+                'name': 'Альмендингер',
             },
             'win_boss_4': {
-                'name': 'Победить Клейст',
-                'id': 4
+                'name': 'Клейст',
             },
             'win_boss_5': {
-                'name': 'Победить Шёрнер',
-                'id': 5
+                'name': 'Шёрнер',
             },
             'win_boss_6': {
-                'name': 'Победить Хубе',
-                'id': 6
+                'name': 'Хубе',
             },
             'win_boss_7': {
-                'name': 'Победить Вёлер',
-                'id': 7
+                'name': 'Вёлер',
             },
             'win_boss_9': {
-                'name': 'Победить Гот',
-                'id': 9
+                'name': 'Гот',
             },
             'win_boss_14': {
-                'name': 'Победить Геббельс',
-                'id': 14
+                'name': 'Геббельс',
             },
             'win_boss_15': {
-                'name': 'Победить Муссолини',
-                'id': 15
+                'name': 'Муссолини',
             }
         }
-
         let damageColor = ['000000', '009600', '009600', '009600', '000096', '000096', 'E69600', 'E69600', 'E69600', 'E69600', 'FF1111', 'FF1111', 'FF0000', 'FF0000', 'FF0000', 'FF0000', 'FF0000', 'FF0000'];
         let weaponsColor = ['000000', '000000', '000000', '000000', '000000', '000096', '000096', '000096', 'E69600', 'E69600', 'E69600', 'FF1111', 'FF0000', 'FF0000'];
-        let sutColor = ['000000', '0000C5', '0000C5', '0000C5', 'E69600', 'FF0000', 'FF0000', 'FF0000', 'FF0000', 'FF0000', 'FF0000', 'FF0000', 'FF0000', 'FF0000', 'FF0000', 'FF0000', 'FF0000']; // таланты
+        let sutColor = ['000000', '0000C5', '0000C5', '0000C5', 'E69600', 'FF0000', 'FF0000', 'FF0000', 'FF0000', 'FF0000', 'FF0000', 'FF0000', 'FF0000', 'FF0000', 'FF0000', 'FF0000', 'FF0000'];
 
-        const myId = function () {
-            if (document.body.innerHTML.match(/\"id\":(\d+),/)) {
-                user = parseInt(document.body.innerHTML.match(/\"id\":(\d+),/)[1]);
+        async function myId() {
+            if (document.body.innerHTML.match(/"id":(\d+),/)) {
+                user = parseInt(document.body.innerHTML.match(/"id":(\d+),/)[1]);
             }
             return user;
         }
 
-        function platform_id_type() {
+        async function platform_id_type() {
             if (document.querySelector('#profile_edit_act')) {
                 return 1;
             }
@@ -92,7 +73,7 @@
             return 0;
         }
 
-        function platform_id_search(type = 0, el = null) {
+        async function platform_id_search(type = 0, el = null) {
             let friends = el;
             switch (type) {
                 case 1:
@@ -123,65 +104,64 @@
             }
         }
 
-        let insertButton = function () {
-            let typeId = platform_id_type();
-
-            console.log(typeId)
-
-            switch (typeId) {
-                case 1:
-                case 2:
-                    let userId = platform_id_search(typeId)
-
-                    if (isNaN(userId) || (userId < 0) || (userId === 0)) {
-                        return;
-                    }
-                    if (document.querySelector('#regiment_check_' + userId)) {
-                        return;
-                    }
-                    document.querySelector('#profile_short').insertAdjacentHTML(
-                        "afterbegin", "<div class='button_wide button_blue clear_fix' id='regiment_check_" + userId + "'><button>Досье на игрока Храбрый Полк</button></div>" +
-                        "<div id='regiment_info" + userId + "' class='post all own post_online'  style='padding:16px 0 19px 3px;'></div>");
-
-                    document.querySelector('#regiment_check_' + userId).addEventListener('click', function () {
-                        userInfo(userId)
-                    });
-                    break;
-                case 3:
-                    document.querySelectorAll('.friends_user_row--fullRow').forEach(function (el) {
-                        if (el.getAttribute("regiment") !== "1") {
-                            let userId = platform_id_search(typeId, el.getAttribute("id"));
-
-                            el.insertAdjacentHTML(
-                                "afterend", "<div class='button_wide button_blue clear_fix' id='regiment_check_" + userId + "'><button>Досье на игрока Храбрый Полк</button></div>" +
-                                "<div id='regiment_info" + userId + "' class='user_block'  style='padding:16px 0 19px 3px;'></div>");
+        function insertButton() {
+            platform_id_type().then(function (typeId) {
+                switch (typeId) {
+                    case 1:
+                    case 2:
+                        platform_id_search(typeId).then(function (userId) {
+                            if (isNaN(userId) || (userId < 0) || (userId === 0)) {
+                                return;
+                            }
+                            if (document.querySelector('#regiment_check_' + userId)) {
+                                return;
+                            }
+                            document.querySelector('#profile_short').insertAdjacentHTML(
+                                "afterbegin", "<div class='button_wide button_blue clear_fix' id='regiment_check_" + userId + "'><button>Досье на игрока Храбрый Полк</button></div>" +
+                                "<div id='regiment_info" + userId + "' class='post all own post_online'  style='padding:16px 0 19px 3px;'></div>");
 
                             document.querySelector('#regiment_check_' + userId).addEventListener('click', function () {
                                 userInfo(userId)
-                            });
-                            el.setAttribute('regiment', 1)
-                        }
-                    })
-                    break;
-                case 4:
-                    document.querySelectorAll('.friends_user_request').forEach(function (value) {
-                        if (value.getAttribute("regiment") !== "1") {
-                            let userId = platform_id_search(typeId, value.querySelector("div[id^='request_controls_']").getAttribute("id"));
-                            value.insertAdjacentHTML(
-                                "afterend", "<div class='button_wide button_blue clear_fix' id='regiment_check_" + userId + "'><button>Досье на игрока Храбрый Полк</button></div>" +
-                                "<div id='regiment_info" + userId + "' class='user_block'  style='padding:16px 0 19px 3px;'></div>");
+                            })
+                        });
+                        break;
+                    case 3:
+                        document.querySelectorAll('.friends_user_row--fullRow').forEach(function (el) {
+                            if (el.getAttribute("regiment") !== "1") {
+                                platform_id_search(typeId, el.getAttribute("id")).then(function (userId) {
+                                    el.insertAdjacentHTML(
+                                        "afterend", "<div class='button_wide button_blue clear_fix' id='regiment_check_" + userId + "'><button>Досье на игрока Храбрый Полк</button></div>" +
+                                        "<div id='regiment_info" + userId + "' class='user_block'  style='padding:16px 0 19px 3px;'></div>");
 
-                            document.querySelector('#regiment_check_' + userId).addEventListener('click', function () {
-                                userInfo(userId)
-                            });
-                            value.setAttribute('regiment', 1)
-                        }
-                    })
-                    break;
-            }
+                                    document.querySelector('#regiment_check_' + userId).addEventListener('click', function () {
+                                        userInfo(userId)
+                                    });
+                                    el.setAttribute('regiment', 1)
+                                })
+                            }
+                        });
+                        break;
+                    case 4:
+                        document.querySelectorAll('.friends_user_request').forEach(function (value) {
+                            if (value.getAttribute("regiment") !== "1") {
+                                platform_id_search(typeId, value.querySelector("div[id^='request_controls_']").getAttribute("id")).then(function (userId) {
+                                    value.insertAdjacentHTML(
+                                        "afterend", "<div class='button_wide button_blue clear_fix' id='regiment_check_" + userId + "'><button>Досье на игрока Храбрый Полк</button></div>" +
+                                        "<div id='regiment_info" + userId + "' class='user_block'  style='padding:16px 0 19px 3px;'></div>");
+
+                                    document.querySelector('#regiment_check_' + userId).addEventListener('click', function () {
+                                        userInfo(userId)
+                                    });
+                                    value.setAttribute('regiment', 1)
+                                })
+                            }
+                        });
+                        break;
+                }
+            });
         }
 
-        function formData(data) {
+        function formData(data = {}) {
             const query = new FormData();
 
             for (let i in data) {
@@ -190,50 +170,52 @@
             return query;
         }
 
-        async function postData(url = '', data = {}, dom) {
-            loader(dom);
-
-            const response = await fetch(url, {
-                method: 'POST',
-                mode: 'cors',
-                body: formData(data)
-            });
-            return await response.json();
+        function printRow(title = '', value = '', xx = 0) {
+            return '<div class="clear_fix"><div class="label fl_l" style="width: 165px; color: #818c99;">' + title + '</div><div class="labeled fl_l" style="word-wrap:break-word;position:relative;overflow:visible;' + (xx ? 'width:345px' : '') + '">' + value + '</div></div>';
         }
 
-        function printRow(title, value, xx) {
-            return '<div class="clear_fix"><div class="label fl_l" style="width: 165px; color: var(--text_secondary);">' + title + '</div><div class="labeled fl_l" style="word-wrap:break-word;position:relative;overflow:visible;' + (xx ? 'width:345px' : '') + '">' + value + '</div></div>';
-        }
-
-        function printTitle(text, a) {
+        function printTitle(text = '', a = '') {
             if (!a) a = '';
             return '<h4 style="height:4px;padding:3px 0 15px 0">' + a + '<b style="padding-right:6px;font-size:11px;background-color:white">' + text + '</b></h4>';
         }
 
-        let loader = function (dom) {
-            dom.innerHTML = "<div width='100%' align='center'><div class='progress_inline'></div></div>";
-        }
-        let userInfo = function (userId) {
-            const regiment = document.querySelector('#regiment_info' + userId);
+        async function loader(dom = {}) {
+            dom.innerHTML = "<div style='text-align: center'><div class='progress_inline'></div></div>";
 
-            postData(getUserInfo, {userId: userId, ownerId: user}, regiment)
-                .then((response) => {
+            return dom;
+        }
+
+        function userInfo(userId = 0) {
+            loader(document.querySelector('#regiment_info' + userId)).then(regiment =>
+                fetch(getUserInfo, {
+                    method: 'POST',
+                    mode: 'cors',
+                    body: formData({userId: userId, ownerId: user})
+                }).then(function (response) {
+                    if (!response.ok) {
+                        throw response;
+                    }
+                    return response.json();
+                }).then(function (response) {
                     if (response.status === 1) {
                         return renderInfo(regiment, response)
                     }
                     regiment.innerHTML = '<div style="text-align: center"><b style="color:#7F0000;font-weight:bold;">' + response.error.messages + '</b></div>';
-                });
+                }).catch(function (error) {
+                    regiment.innerHTML = '<div style="text-align: center"><b style="color:#7F0000;font-weight:bold;">Ошибка при подключение к серверу скрипта.</b></div>';
+                })
+            );
         }
 
-        let renderInfo = function (regiment, response) {
+        function renderInfo(regiment = {}, response = {}) {
             let data = response.result.data;
+
             let html = printRow('ID: ', data.platform_id);
             html += printRow('Уровень: ', data.level);
             html += printRow('Сут: ', '<b  style="color:#' + sutColor[data.achievements.sut] + ';font-size:10px;background:#fff">' + data.sut + ' </b>');
             html += printRow('Боевые достижения: ', '' +
                 '<b  style="color:#' + damageColor[data.totalDamage] + ';font-size:10px;background:#fff">' + intToString(achievements['damage'][data.totalDamage]) + ' урона, </b>' +
                 '<b  style="color:#' + weaponsColor[data.achievements.tokens] + ';font-size:10px;background:#fff">' + intToString(achievements['tokens'][data.achievements.tokens]) + ' жетонов, </b>' +
-                '<b  style="color:#' + weaponsColor[data.achievements.weapon_5] + ';font-size:10px;background:#fff">' + intToString(achievements['weapons'][data.achievements.weapon_5]) + ' бронейбойных, </b>' +
                 '<b  style="color:#' + weaponsColor[data.achievements.weapon_6] + ';font-size:10px;background:#fff">' + intToString(achievements['weapons'][data.achievements.weapon_6]) + ' кумулятивных</b>'
             );
             html += printRow('Таланты: ', data.usedTalents);
@@ -273,7 +255,7 @@
             });
         }
 
-        function intToString(value) {
+        function intToString(value = '') {
             let str = reverse_string(value.toString()).replace(/000/gi, "k");
             let out = reverse_string(str);
             if (parseInt(value) > 0) {
@@ -282,7 +264,7 @@
             return out;
         }
 
-        function reverse_string(str) {
+        function reverse_string(str = '') {
             let out = "";
             for (let i = str.length - 1; i >= 0; i--) {
                 out += str[i];
@@ -290,7 +272,7 @@
             return out;
         }
 
-        function tsFormat(ts) {
+        function tsFormat(ts = 0) {
             let m = JSON.parse('{"Jan":"01", "Feb":"02", "Mar":"03", "Apr":"04", "May":"05", "Jun":"06", "Jul":"07", "Aug":"08", "Sep":"09", "Oct":"10", "Nov":"11", "Dec":"12"}');
             let d = new Date();
             d.setTime(ts.toString().length > 10 ? ts : ts * 1000);
@@ -298,8 +280,8 @@
             return a[2] + '.' + m[a[1]] + '.' + a[3] + ' в ' + a[4];
         }
 
-        if (myId() > 0) {
-            setInterval(insertButton, 300);
-        }
+        myId().then(r =>
+            setInterval(insertButton, 300)
+        )
     }
 )();

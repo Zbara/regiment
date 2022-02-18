8b4fa790b739588d839c4b9278b0dfaee087060d
Stas <zbara.dev@gmail.com>
2022-02-18 16:57:29 +0200

    offerta

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
 
 class MainController extends AbstractController
 {
@@ -12,5 +13,13 @@ class MainController extends AbstractController
     public function index(): Response
     {
         return new Response('Работает! Symfony prod.');
+    }
+
+    #[
+        Route('/contract-offer', name: 'contract-offer')
+    ]
+    public function contract(Security $security): Response
+    {
+        return $this->render('main/contract-offer.html.twig');
     }
 }
